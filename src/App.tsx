0daaaa9f3105d250d100c4b81e8091e19d8130dd
Miha Drofenik <miha.drofenik@visionect.com>
2022-07-03 16:28:34 +0200

    test env prepared

--- src/App.tsx
+++ src/App.tsx
@@ -54,30 +54,30 @@ function App({ putGoogleUser }: IProps) {
     )
   }
 
-  if (PRODUCTION && !loggedIn) {
-    return (
-      <div className="container-fluid">
-        <span className="fixed-middle d-flex flex-column justify-content-center text-center">
-          <h2 className="mb-3">Trello API integration</h2>
-          <div className="mb-3">
-            <GoogleLogin
-              clientId="30269258381-dj8lnlf7ouintma2bpgo58nm97fsas00.apps.googleusercontent.com"
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              onRequest={startLoadingGoogle}
-              isSignedIn={true}
-              cookiePolicy={'single_host_origin'}
-            />
-          </div>
-          <div>
-            <Typography align="center" variant="body2">
-              {error}
-            </Typography>
-          </div>
-        </span>
-      </div>
-    )
-  }
+  // if (PRODUCTION && !loggedIn) {
+  //   return (
+  //     <div className="container-fluid">
+  //       <span className="fixed-middle d-flex flex-column justify-content-center text-center">
+  //         <h2 className="mb-3">Trello API integration</h2>
+  //         <div className="mb-3">
+  //           <GoogleLogin
+  //             clientId="30269258381-dj8lnlf7ouintma2bpgo58nm97fsas00.apps.googleusercontent.com"
+  //             onSuccess={responseGoogle}
+  //             onFailure={responseGoogle}
+  //             onRequest={startLoadingGoogle}
+  //             isSignedIn={true}
+  //             cookiePolicy={'single_host_origin'}
+  //           />
+  //         </div>
+  //         <div>
+  //           <Typography align="center" variant="body2">
+  //             {error}
+  //           </Typography>
+  //         </div>
+  //       </span>
+  //     </div>
+  //   )
+  // }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
